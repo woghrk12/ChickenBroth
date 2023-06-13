4545e5beb31b328a68d2cdb525f6f1fda4608777
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-06-14 01:19:59 +0900

    Added premake and generated projects

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "ChickenBroth"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "ChickenBroth"
+    location "ChickenBroth"
+    kind "SharedLib"
+    language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CB_PLATFORM_WINDOWS",
+			"CB_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CB_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"ChickenBroth/vendor/spdlog/include",
+		"ChickenBroth/src"
+	}
+
+	links
+	{
+		"ChickenBroth"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CB_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CB_DIST"
+		optimize "On"
