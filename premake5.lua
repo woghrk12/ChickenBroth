682d290b36778f3741af5c7f234086b7a8c53035
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-06-27 21:26:00 +0900

    Postbuild command for ChickenBroth project modified

--- premake5.lua
+++ premake5.lua
@@ -72,7 +72,7 @@ project "ChickenBroth"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
