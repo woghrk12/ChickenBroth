cecc5c0f536dc63a9f47d881a2912bb5dfc70bfb
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-07-18 22:04:45 +0900

    Changed macro name (FATAL -> CRITICAL)

--- ChickenBroth/src/ChickenBroth/Log.h
+++ ChickenBroth/src/ChickenBroth/Log.h
@@ -21,15 +21,15 @@ namespace ChickenBroth
 }
 
 // Core log macros
-#define CB_CORE_TRACE(...)    ::ChickenBroth::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define CB_CORE_INFO(...)     ::ChickenBroth::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define CB_CORE_WARN(...)     ::ChickenBroth::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define CB_CORE_ERROR(...)    ::ChickenBroth::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CB_CORE_FATAL(...)    ::ChickenBroth::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define CB_CORE_TRACE(...)		::ChickenBroth::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CB_CORE_INFO(...)		::ChickenBroth::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CB_CORE_WARN(...)		::ChickenBroth::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CB_CORE_ERROR(...)		::ChickenBroth::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CB_CORE_CRITICAL(...)	::ChickenBroth::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
-#define CB_TRACE(...)	      ::ChickenBroth::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define CB_INFO(...)	      ::ChickenBroth::Log::GetClientLogger()->info(__VA_ARGS__)
-#define CB_WARN(...)	      ::ChickenBroth::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define CB_ERROR(...)	      ::ChickenBroth::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CB_FATAL(...)	      ::ChickenBroth::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define CB_TRACE(...)			::ChickenBroth::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CB_INFO(...)			::ChickenBroth::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CB_WARN(...)			::ChickenBroth::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CB_ERROR(...)			::ChickenBroth::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CB_CRITICAL(...)		::ChickenBroth::Log::GetClientLogger()->critical(__VA_ARGS__)
