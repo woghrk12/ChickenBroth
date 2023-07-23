c8864163599b07f257b88f0a0e6514c57c44dd57
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2023-07-23 21:15:26 +0900

    Added transforms to Renderer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public ChickenBroth::Layer
 {
 public:
@@ -16,6 +18,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -25,7 +28,7 @@ public:
 				v_Position = a_Position;
 				v_Color = a_Color;
 
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -82,6 +85,7 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			
@@ -89,7 +93,7 @@ public:
 			{
 				v_Position = a_Position;
 
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -114,10 +118,10 @@ public:
 		// Square Vertex Buffer
 		float squareVertices[3 * 4] =
 		{
-			-0.75f, -0.75f, 0.0f,
-			0.75f, -0.75f, 0.0f,
-			0.75f, 0.75f, 0.0f, 
-			-0.75f, 0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.5f, 0.5f, 0.0f, 
+			-0.5f, 0.5f, 0.0f
 		};
 
 		std::shared_ptr<ChickenBroth::VertexBuffer> squareVB;
@@ -156,7 +160,19 @@ public:
 
 		ChickenBroth::Renderer::BeginScene(m_Camera);
 
-		ChickenBroth::Renderer::Submit(m_SquareShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				
+				ChickenBroth::Renderer::Submit(m_SquareShader, m_SquareVA, transform);
+			}
+		}
+
 		ChickenBroth::Renderer::Submit(m_TriangleShader, m_TriangleVA);
 
 		ChickenBroth::Renderer::EndScene();
